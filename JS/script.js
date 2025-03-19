206e75a6e99085e0987b2b2dee26a9f7728c3d70
Leandro Andere <Leangamer25@gmail.com>
2025-03-19 11:17:03 -0300

    se agrega array y corrige errores

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,34 @@
 
 let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
+let stockTotal = [
+    {name:"ANGEL FANTASM" , price: 1200 , img: "assets/images/ANGEL-FANTASM-MUGLER.jpg" },
+    {name:"dune edt" , price: 799 , img:"assets/images/dune-edt.jpg" },
+    {name:"green tea fig" , price: 3299 , img:"assets/images/green-tea-fig-edt.jpg" },
+    {name:"IDOLE L'INTENSE" , price: 4999 , img:"assets/images/IDOLE-L'INTENSE-EDP-75ML.jpg" },
+    {name:"in2u men" , price: 999 , img: "assets/images/in2u-men-edt.jpg" },
+    {name:"iris edp cher" , price: 599 , img:"assets/images/iris-edp-cher.jpg" },
+    {name:"k by dolcegabbana" , price: 3599 , img:"assets/images/k-by-dolce_gabbana-edt.jpg" },
+    {name:"L'EAU D'ISSEY" , price: 2499 , img:"assets/images/L'EAU-D'ISSEY-POUR-HOMME.jpg" }
+]
+    const nombre = document.getElementsByClassName('product-name');
+    const precio = document.getElementsByClassName('price');
+    const imagen = document.getElementsByClassName('img');
+    const btn = document.getElementsByClassName('btnComprar');
+
+    
+    stockTotal.forEach(function(stock,index) {
+        if (nombre[index]) nombre[index].textContent = `${stock.name}`;
+        if (precio[index]) precio[index].textContent = `$${stock.price}`;
+        if (imagen[index]) imagen[index].src = `${stock.img}`;
+
+        if (btn[index]) {
+            btn[index].addEventListener('click', function() {
+                agregarProducto(stock.name, stock.price);
+            });
+        }
+    });
+
 
 function agregarProducto(nombre, precio) {
     
@@ -33,7 +61,6 @@ function actualizarTabla() {
     const tabla = document.getElementById('tablaCarrito').getElementsByTagName('tbody')[0];
     tabla.innerHTML = '';
 
-
             carrito.forEach((producto , index) => {
         const row = tabla.insertRow();
 
@@ -62,7 +89,7 @@ function actualizarTotal() {
     });
 
     const totalInput = document.querySelector('.total');
-    totalInput.value = `${total.toFixed(2)}`;
+    totalInput.value = `$${total.toFixed(2)}`;
     
 }
 
